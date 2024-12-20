bc4e09c3cfb8e730a5c8e7f865dc358a892f38df
James Aylward <james.michael.aylward@gmail.com>
2024-12-20 16:07:00 +1000

    finished part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,38 @@
 use std::{fs::File, io::{BufRead, BufReader}};
 
+fn find_cross(
+    grid: &Vec<Vec<char>>,
+    row: usize,
+    column: usize,
+    directions: &[(i32, i32)]
+) -> bool {
+
+    if grid[row][column] != 'A' {
+        return false;
+    }
+
+    let letters = directions.iter().map(|dir| {
+        if let Some((x, y)) = get_position(row, column, &dir, 1) {
+            grid.get(y as usize)
+                .and_then(|row: &Vec<char>| row.get(x as usize).copied())
+        } else { None }
+    }).collect::<Vec<_>>();
+
+    let a: String = letters.iter()
+        .take(2)
+        .map(|x| x.unwrap_or('_'))
+        .collect();
+
+    let b: String = letters.iter()
+        .rev()
+        .take(2)
+        .map(|x| x.unwrap_or('_'))
+        .collect();
+
+    let combos = ["MS", "SM"];
+    combos.contains(&a.as_str()) && combos.contains(&b.as_str())
+}
+
 fn get_position(
     row: usize,
     column: usize,
@@ -52,14 +85,16 @@ fn main() -> anyhow::Result<()> {
         .map(|l| l.unwrap().chars().collect::<Vec<_>>() )
         .collect::<Vec<_>>();
 
-    let directions = [
+    let orthogonals = [
         (-1, 0), // back
         (1, 0), // forwards
         (0, 1), // up
         (0, -1), // down
-        (1, 1), // top right
+    ];
+    let diagonals = [
         (-1, 1), // top left
         (1, -1), // bottom right
+        (1, 1), // top right
         (-1, -1), // bottom left
     ];
 
@@ -68,11 +103,12 @@ fn main() -> anyhow::Result<()> {
 
     for r in 0..grid.len() {
         for c in 0..grid[r].len() {
-            count_a += directions
+            count_a += [orthogonals, diagonals].concat()
                 .iter()
                 .map(|dir| find_word(&grid, &dir, r, c))
                 .fold(0, |acc, x| acc + x as u32);
-            }
+            count_b += find_cross(&grid, r, c, &diagonals) as u32;
+        }
     }
 
     println!("Count is {}", count_a);
@@ -80,4 +116,3 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
-
